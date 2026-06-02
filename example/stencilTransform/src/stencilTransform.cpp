7c6e36d9591559a2b46b129507f5136f1ea4da55
Simeon Ehrig <s.ehrig@hzdr.de>
2026-06-02 12:43:39 +0200

    WIP: I think I will never finish this
    - it does not compile

--- example/stencilTransform/src/stencilTransform.cpp
+++ example/stencilTransform/src/stencilTransform.cpp
@@ -8,13 +8,45 @@
 
 using namespace alpaka;
 
+using Vec2D = Vec<std::size_t, 2>;
+
 struct GameOfLife
 {
     constexpr auto operator()(concepts::SimdPtr auto const& elem) const
     {
-        return elem.load() + 1;
+        using SimdType = typename ALPAKA_TYPEOF(elem.load());
+        auto sum = elem[Vec2D{-1, -1}].load() + elem[Vec2D{-1, 0}].load() + elem[Vec2D{-1, 1}].load()
+                   + elem[Vec2D{0, -1}].load() + elem[Vec2D{0, 1}].load() + elem[Vec2D{1, -1}].load()
+                   + elem[Vec2D{1, 0}].load() + elem[Vec2D{1, 1}].load();
+
+        if(where(elem.load() == SimdType::fill(1), true))
+        {
+            return 0;
+        }
+        return elem.load();
     }
 };
+
+void print_world(alpaka::concepts::IDataSource auto const& world)
+{
+    static_assert(ALPAKA_TYPEOF(world)::dim() == 2);
+    for(std::size_t y = 0; y < world.getExtents()[0]; ++y)
+    {
+        for(std::size_t x = 0; x < world.getExtents()[1]; ++x)
+        {
+            if(world[Vec{y, x}] == 0)
+            {
+                std::cout << ". ";
+            }
+            else
+            {
+                std::cout << "x ";
+            }
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
 
 auto example(auto const deviceSpec, auto const exec) -> int
 {
@@ -23,43 +55,41 @@ auto example(auto const deviceSpec, auto const exec) -> int
     // We use a square world. Each site has the size.
     constexpr std::size_t site_size = 10;
     // Add 2 to each site for the halo
-    constexpr Vec<std::size_t, 2> extents{site_size + 2, site_size + 2};
+    constexpr Vec2D extents{site_size + 2, site_size + 2};
 
     auto deviceSelector = onHost::makeDeviceSelector(deviceSpec);
     onHost::Device device = deviceSelector.makeDevice(0);
     onHost::Queue queue = device.makeQueue();
 
-    auto bufHost = onHost::allocHost<int>(extents);
-    auto bufDevice = onHost::alloc<int>(device, extents);
-    onHost::memset(queue, bufDevice, 0);
 
-    onHost::transform(queue, exec, bufDevice, StencilFunc{GameOfLife{}}, bufDevice);
-    onHost::memcpy(queue, bufHost, bufDevice);
+    auto bufHost = onHost::allocHost<int>(Vec2D{site_size, site_size});
+    onHost::memset(queue, bufHost, 0);
     onHost::wait(queue);
 
-    std::cout << "Only user data: \n";
-    for(std::size_t y = 1; y < site_size + 1; ++y)
-    {
-        for(std::size_t x = 1; x < site_size + 1; ++x)
-        {
-            std::cout << bufHost[Vec{y, x}] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
+    // .x.
+    // ..x
+    // xxx
+    bufHost[Vec2D{0, 1}] = 1;
+    bufHost[Vec2D{1, 2}] = 1;
+    bufHost[Vec2D{2, 0}] = 1;
+    bufHost[Vec2D{2, 1}] = 1;
+    bufHost[Vec2D{2, 2}] = 1;
 
-    std::cout << "User data with Halo: \n";
-    for(std::size_t y = 0; y < site_size + 2; ++y)
-    {
-        for(std::size_t x = 0; x < site_size + 2; ++x)
-        {
-            std::cout << bufHost[Vec{y, x}] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
+    print_world(bufHost);
 
+    auto bufA = onHost::alloc<int>(device, Vec2D{site_size + 2, site_size + 2});
+    auto bufB = onHost::alloc<int>(device, Vec2D{site_size + 2, site_size + 2});
+    auto coreViewA = bufA.getSubView(Vec2D{1, 1}, Vec2D{site_size, site_size});
+    auto coreViewB = bufB.getSubView(Vec2D{1, 1}, Vec2D{site_size, site_size});
 
+    onHost::memset(queue, bufB, 0);
+    onHost::memcpy(queue, coreViewA, bufHost);
+
+    onHost::transform(queue, exec, coreViewB, StencilFunc{GameOfLife{}}, coreViewA);
+    onHost::memcpy(queue, bufHost, coreViewB);
+    onHost::wait(queue);
+
+    print_world(bufHost);
     return 0;
 }
 
