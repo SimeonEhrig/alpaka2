c3475506d21d07bba87db3c80369cf2ef239f64b
Simeon Ehrig <s.ehrig@hzdr.de>
2026-09-15 12:47:26 +0200

    CI: update the patch level of the CMake versions to match pre-installed CMake version
    - the alpaka-group-container provides pre-installed CMake versions. If
      we match the patch level, we can use the preinstalled versions instead
      pull a CMake version each time.

--- script/job_generator/src/alpaka_bashi/jobs_builder/emulated_simd.py
+++ script/job_generator/src/alpaka_bashi/jobs_builder/emulated_simd.py
@@ -64,7 +64,7 @@ def get_emulated_simd_job(
             [
                 (HOST_COMPILER, compiler_name, compiler_version),
                 (DEVICE_COMPILER, compiler_name, compiler_version),
-                (CMAKE, "3.30.3"),
+                (CMAKE, "3.30.9"),
                 (UBUNTU, "24.04"),
                 (CXX_STANDARD, 20),
                 (BUILD_TYPE, CMAKE_DEBUG),
