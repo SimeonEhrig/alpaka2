270acbf448942c7667f2cef0bdc592c086fb1fd8
René Widera <r.widera@hzdr.de>
2026-09-16 09:46:27 +0200

    CI: add cuda 13.4 support (#681)
    * CI: add CUDA 13.4 support
    
    * simplify CUDA installer script
    
    Use a lookup array to match CUDA version number and the excact number to
    download the dep package.
    
    * restrict nvcc 13.4 supported CUDA version
    
    * CI: cuda.sh improve check for ubuntu version
    
    - add some comments
    
    ---------
    
    Co-authored-by: Simeon Ehrig <s.ehrig@hzdr.de>

--- script/job_generator/src/alpaka_bashi/versions.py
+++ script/job_generator/src/alpaka_bashi/versions.py
@@ -31,6 +31,7 @@ from bashi.globals import (
 )
 from bashi.version.dependencies.base_version_support import ClangBase
 from bashi.version.dependencies.clang_cuda import CLANG_CUDA_MAX_CUDA_VERSION, ClangCudaSDKSupport
+from bashi.version.dependencies.nvcc import NvccHostSupport, NVCC_GCC_MAX_VERSION
 from bashi.version.dependencies.hipcc import HIPCC_CLANG_VERSION
 
 from alpaka_bashi.globals import BUILD_TYPE, BUILD_TYPES, HWLOC
@@ -38,7 +39,7 @@ from alpaka_bashi.globals import BUILD_TYPE, BUILD_TYPES, HWLOC
 ALPAKA_VERSIONS: dict[str, list[str | int | float]] = {
     GCC: [12, 13, 14, 15],
     CLANG: [17, 18, 19, 20, 21],
-    NVCC: [12.5, 12.6, 12.8, 12.9, 13.0, 13.1, 13.2, 13.3],
+    NVCC: [12.5, 12.6, 12.8, 12.9, 13.0, 13.1, 13.2, 13.3, 13.4],
     HIPCC: [6.3, 6.4, 7.0, 7.1, 7.2, 7.14],
     ICPX: ["2025.1", "2025.2", "2025.3", "2026.0", "2026.1"],
     UBUNTU: ["24.04"],
@@ -161,6 +162,13 @@ def get_alpaka_version_relation() -> bashi.VersionRelation:
         ClangCudaSDKSupport("20", "12.8"),
         ClangCudaSDKSupport("22", "13.0"),
     ]
+
+    # Supported compilers:
+    # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#host-compiler-support-policy
+    nvcc_gcc_max_version = NVCC_GCC_MAX_VERSION + [
+        NvccHostSupport("13.4", "16"),
+    ]
+
     hipcc_clang_version = HIPCC_CLANG_VERSION + [
         ClangBase("7.14", "23"),
     ]
@@ -168,4 +176,5 @@ def get_alpaka_version_relation() -> bashi.VersionRelation:
     return bashi.VersionRelation(
         clang_cuda_max_cuda_version=clang_cuda_max_cuda_version,
         hipcc_clang_version=hipcc_clang_version,
+        nvcc_gcc_max_version=nvcc_gcc_max_version,
     )
